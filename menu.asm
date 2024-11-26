685b3fa209a004cb24e202d39a1140c2ebb7ecb0
Lucasfz25 <lucasfz2525@gmail.com>
2024-11-26 20:55:51 -0300

    Agrego un menu al iniciar el programa

---
+++ menu.asm
@@ -0,0 +1,74 @@
+; menu de inicio del programa
+
+%include "constantes.asm"
+
+global menu
+extern printf, gets, puts
+
+
+section .data
+    
+    mensaje_inicio      db  'Bienvenido a El Asalto', 0
+    mensaje_solicitar_accion    db  'Ingrese una accion:', 0
+    mensaje_opcion_1    db  'nueva - Iniciar una nueva partida', 0
+    mensaje_opcion_2    db  'cargar - Carga una partida guardada previamente', 0
+    mensaje_opcion_3    db  'salir - Salir del programa', 0
+    mensaje_accion_invalida db  'Ingrese una accion valida:', 0
+    accion              resb    100
+    accion1             db  'nueva', 0
+    accion2             db  'cargar', 0
+    accion3             db  'salir', 0
+
+
+section .text
+
+menu:
+
+    push rbx
+    push r12
+
+    mov     rdi,mensaje_inicio
+    sub     rsp,8
+    call puts
+    add     rsp,8
+
+acciones:
+    mov     rdi,mensaje_opcion_1
+    sub     rsp,8
+    call puts
+    mov     rdi,mensaje_opcion_2
+    call puts
+    mov     rdi,mensaje_opcion_3
+    call puts
+    mov     rdi,mensaje_solicitar_accion
+    call puts
+    add     rsp,8
+
+leer_accion:
+    mov rdi,accion
+    call gets
+    mov rax,[accion]
+    cmp eax,[accion1]
+    je nuevo_juego
+    cmp eax,[accion2]
+    je cerrar_programa
+    cmp eax,[accion3]
+    je cerrar_programa
+
+accion_invalida:
+    mov     rdi,mensaje_accion_invalida
+    sub     rsp,8
+    call    puts
+    add     rsp,8
+    jmp leer_accion
+
+
+nuevo_juego:
+    pop r12
+    pop rbx
+    ret
+
+cerrar_programa:
+    mov rax, 60     ; syscall exit
+    xor rdi, rdi    ; estado de salida 0
+    syscall
